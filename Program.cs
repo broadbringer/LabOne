1584929093116389d3b13cfc654f7fcb549362cd
oleksandr.tokar <oleksand.tokar@bini.games>
2021-04-27 18:36:51 +0300

    second commint

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using LabOne.Interfaces;
+using LabOne.State;
+
+namespace LabOne
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mainProgram = new MainProgram(new FirstEnter());
+            mainProgram.ShowInfo();
+        }
+    }
+
+    public class MainProgram
+    {
+        public IProgramState State;
+
+        public MainProgram(IProgramState state)
+        {
+            State = state;
+        }
+
+        public void ShowInfo()
+        {
+            State.ShowInfo(this);
+        }
+
+        public void WaitInput()
+        {
+            
+        }
+    }
+}
